8bbb5329602fc0b74b2f64a368b73266db6a775d
nripesh.agarwal <nripesh.agarwal@gmail.com>
2019-12-06 09:59:47 +0530

    added new token

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 discord_bot_name="bs_test"
 discord_client_id = "650796075887624205"
-
 discord_token = "NjUwNzk2MDc1ODg3NjI0MjA1.XenMTw.QbLDT9FGFi6PO-VazlRQkvDSKlU"
 
 GOOGLE_API = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBuqg3J-d965r5GBxcJUgkZsJst58lcupg&cx=000637338422175236148:qau5q9golx0"
